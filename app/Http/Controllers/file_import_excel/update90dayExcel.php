4407b6c0f300ecd4923d9121a507d33c00eb1aa6
anucha1993 <108377127+anucha1993@users.noreply.github.com>
2024-02-14 13:40:13 +0700

    Update update90dayExcel.php

--- app/Http/Controllers/file_import_excel/update90dayExcel.php
+++ app/Http/Controllers/file_import_excel/update90dayExcel.php
@@ -49,7 +49,7 @@ class update90dayExcel extends Controller
        foreach ($request->labour_passport_number as $key => $v)
        {
         //labour
-        LabourModel::where('labour_passport_number',$request->labour_passport_number[$key])
+         LabourModel::where('labour_passport_number',$request->labour_passport_number[$key])
         ->update([
             'labour_ninety_date_start' => date('Y-m-d', strtotime($request->labour_ninety_date_start[$key])),
             'labour_ninety_date_end'   => date('Y-m-d', strtotime($request->labour_ninety_date_end[$key])),
@@ -64,9 +64,27 @@ class update90dayExcel extends Controller
             'ninety_note'      => $request->ninety_note[$key],
             'ninety_user_add'  => Auth::user()->name,
         ]);
+
+
         $excelData =NULL;
         $labours =  LabourModel::select('labour_passport_number')->where('labour_passport_number',$request->labour_passport_number[$key])->first();
         $passP[] = $labours->labour_passport_number;
+
+        
+         //บันทึก Log
+         activity()
+         ->performedOn($labours)
+         ->tap(function ($activity) use ($labours) {
+             // กำหนดข้อมูลในฟิลด์ที่ต้องการ
+             $activity->causer_type  = Auth::user()->name;
+             $activity->log_name     = 'labour';
+             $activity->subject_type = 'import-excel';
+             $activity->event        = 'update90day';
+             $activity->properties   = $labours;
+         })
+         ->log($labours->labour_name . ',' . $labours->labour_passport_number);
+
+
        }
        $result = LabourModel::whereIn('labour_passport_number',$passP)->leftJoin('nationality', 'nationality.nationality_id', '=', 'labour.labour_nationality')->get();
       // dd($result);
